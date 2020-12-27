d964071477f63a636bdf620d545a1a39b7b372b1
Sean F Quinn <sean@esqew.com>
2020-12-27 15:33:03 -0500

    Initial commit prior to publishing on NPM

---
+++ README.md
@@ -0,0 +1,25 @@
+# node-kenpom
+Abstracts the scraping of the team ratings from the KenPom.com homepage into a convenient module.
+
+## Important information
+Use this package **AT YOUR OWN RISK**. The creator nor the maintainers of this package make any guarantee as to the package's functionality, and you are solely responsible for what you use this package for.
+
+## Installation
+Install the module from this repository by running the following in your project directory:
+
+    npm install git+https://github.com/esqew/node-kenpom.git
+
+## Usage
+Once installed, import the module in your code, then call the `getData()` method:
+
+    const kenpom = require('node-kenpom');
+    const ratings = await kenpom.getData();
+
+### Return object
+The module packages up the extracted data into a neat Object for further consumption. Most fields are named as they appear on the KenPom.com page, with some exceptions:
+
+    ratings.scrapeTime; // a Date object representing the time the scrape completed
+    ratings.asOfString; // the string displayed above the ratings table at the time of the scrape (useful to know if all the data from the day is reflected in the result set)
+
+    ratings.teams[0].AdjEM; // gets the AdjEM metric for the first team in the list (#1 rank)
+    ratings.teams[1].NCSoS_AdjEM; // gets the NCSOS AdjEM metric for the second team in the list (#2 rank)
---
+++ node-kenpom.js
@@ -0,0 +1,91 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+/**
+ * getData
+ * @description Retrieves the latest KenPom.com ratings and parses them to an Array of Objects for further consumption
+ * @returns {Object} An Object containing information about the scrape, as well as the scraped data.
+ */
+const getData = async () => {
+    return new Promise((resolve) => {
+        axios.get('https://kenpom.com')
+            .then(result => {
+                const scrapeTime = new Date();
+                const $ = cheerio.load(result.data);
+                var teams = [];
+                var headerCategoryCells = $("#ratings-table thead tr.thead1")[0].children.filter(_ => _.type === 'tag' && _.name == 'th');
+                var headerCategories = [];
+
+                // get the header "categories" (like "Strength of Schedule" and "NCSOS") that are listed in a separate <tr> node
+                for (categoryCell of headerCategoryCells) {
+                    var numberOfColumns;
+                    if (typeof categoryCell.attribs !== 'undefined' && typeof categoryCell.attribs.colspan !== 'undefined') {
+                        if (typeof categoryCell.attribs.class !== 'undefined' && categoryCell.attribs.class.includes("divide")) {
+                            numberOfColumns = parseInt(categoryCell.attribs.colspan) / 2;
+                        } else {
+                            numberOfColumns = parseInt(categoryCell.attribs.colspan);
+                        }
+                    } else {
+                        numberOfColumns = 1;
+                    }
+                    headerCategories.push(...new Array(numberOfColumns).fill().map(_ => $(categoryCell).text().replace('Strength of Schedule', 'SoS')));
+                }
+
+                // get the text of the actual header labels and concatenate with the header "categories" (if applicable)
+                var headers = $("#ratings-table thead tr.thead2")[0].children.filter(_ => _.type === 'tag').map((header, index) => headerCategories[index] !== "" ? headerCategories[index] + "_" + $(header).text() : $(header).text());
+
+                // loop over each of the team rows' data and generate an Object using the extracted headers
+                for (team of $("#ratings-table tbody tr")) {
+                    var teamObject = {};
+                    var teamData = team.children.filter(element =>
+                        element.type == 'tag' &&
+                        element.name == 'td' &&
+                        (
+                            element.attribs.class == undefined ||
+                            !element.attribs.class.includes('td-right')
+                        )
+                    );
+                    teamData.map(element => $(element).text()).forEach((value, index) => teamObject[headers[index]] = value)
+                    teams.push(teamObject);
+                }
+
+                // parse numeric values to their respective types, if possible
+                for (team of teams) {
+                    for (key of Object.keys(team)) {
+                        if (/^(\+|\-)?\d{0,3}\.\d{1,3}$/.test(team[key])) team[key] = parseFloat(team[key]);
+                        else if (/^\d{1,3}$/.test(team[key])) team[key] = parseInt(team[key]);
+                        else if (/^\d{1,2}\-\d{1,2}$/.test(team[key])) team[key] = new Record(team[key]);
+                    }
+                }
+                resolve({
+                    scrapeTime: scrapeTime,
+                    asOfString: $("span.update").text(),
+                    teams:      teams
+                });
+                return;
+            })
+    });
+};
+
+class Record {
+    constructor(recordString) {
+        const pattern = /^(?<wins>\d{1,2})\-(?<losses>\d{1,2})$/;
+        if (!recordString instanceof String) throw new TypeError("Record must be constructed using a String representation of a team's record in the format ##-##.");
+        else if (!pattern.test(recordString)) throw new Error("Record expects a record string in the format ##-##.");
+        else {
+            const parseResult = pattern.exec(recordString);
+            this.wins = parseResult.groups.wins;
+            this.losses = parseResult.groups.losses;
+        }
+    }
+
+    toString() {
+        return `${this.wins}-${this.losses}`;
+    }
+}
+
+module.exports = {getData, Record};
+
+(async () => {
+    console.dir(await getData());
+})()
